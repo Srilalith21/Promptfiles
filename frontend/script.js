e3e09ead76317902543a688df827e2b0bf3e4261
Srilalith <sri21@gmail.com>
2026-02-26 06:28:42 +0530

    Update Codebase with structured folders

---
+++ frontend/script.js
@@ -0,0 +1,33 @@
+let data;
+
+function getFormData() {
+  data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    date: document.getElementById("date").value.toString(),
+    seats: (document.getElementById("guests").selectedIndex)
+  };
+  postToServer(data);
+}
+
+function postToServer(data) {
+  if (data.name == "") {
+    console.log("Operaion failed");
+    return;
+  }
+  console.log(data)
+  const server = fetch("http://localhost:9090/formdata", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json", // Indicate the content type
+    },
+    body: JSON.stringify(data),
+  });
+  server
+    .then((res) => {
+      console.log(res.statusText);
+    })
+    .catch((res) => {
+      console.log(res);
+    });
+}
